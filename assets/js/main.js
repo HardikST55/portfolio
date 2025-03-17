6f9d134fcef4a606b113636eaa28504cc96d16bb
Hardik Singh Thakuri <hardiksinghthakuri55@gmail.com>
2025-03-18 00:11:58 +0545

    final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,6 +136,6 @@ const sr = ScrollReveal({
     // reset: true // Animations repeats
 })
 
-sr.reveal(".home__perfil, .about__image, .contact__mail", {origin: 'right'})
-sr.reveal(".home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data", {origin: 'left'})
+sr.reveal(".home__perfil, .about__image,  .contact__social, .contact__data", {origin: 'right'})
+sr.reveal(".home__name, .home__info, .about__container .section__title-1, .about__info, .contact__mail", {origin: 'left'})
 sr.reveal(".services__card, .projects__card", {interval: 100})
